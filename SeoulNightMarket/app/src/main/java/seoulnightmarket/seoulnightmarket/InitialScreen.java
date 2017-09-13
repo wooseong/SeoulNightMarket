38711c17190ced66042da6e131de724fa6cd8166
Yookmoonsu <yookmoon@naver.com>
2017-09-14 01:37:56 +0900

    메인화면에 action bar 생성 및 search view 추가

---
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/InitialScreen.java
@@ -0,0 +1,41 @@
+package seoulnightmarket.seoulnightmarket;
+
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class InitialScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_screen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { // 액션버튼 메뉴 액션바에 넣기
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.searchview_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) { // 검색어 완료시
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) { // 검색어 입력할 때
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
