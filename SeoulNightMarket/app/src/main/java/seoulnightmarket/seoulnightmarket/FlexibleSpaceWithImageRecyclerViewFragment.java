ebc4004d81d0536fdbd424695b759339df4549bd
DESKTOP-0IINLS2\lisa9 <lisa960310@naver.com>
2017-09-14 03:58:58 +0900

    오픈소스 에러 다 잡음 우리가 원하는 방식으로 고치는 과정해야함

---
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/FlexibleSpaceWithImageRecyclerViewFragment.java
@@ -0,0 +1,98 @@
+package seoulnightmarket.seoulnightmarket;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.nineoldandroids.view.ViewHelper;
+
+public class FlexibleSpaceWithImageRecyclerViewFragment extends FlexibleSpaceWithImageBaseFragment<ObservableRecyclerView> {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_flexiblespacewithimagerecyclerview, container, false);
+
+        final ObservableRecyclerView recyclerView = (ObservableRecyclerView) view.findViewById(R.id.scroll);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setHasFixedSize(false);
+        final View headerView = LayoutInflater.from(getActivity()).inflate(R.layout.recycler_header, null);
+        final int flexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
+        headerView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, flexibleSpaceImageHeight));
+        setDummyDataWithHeader(recyclerView, headerView);
+
+        // TouchInterceptionViewGroup should be a parent view other than ViewPager.
+        // This is a workaround for the issue #117:
+        // https://github.com/ksoichiro/Android-ObservableScrollView/issues/117
+        recyclerView.setTouchInterceptionViewGroup((ViewGroup) view.findViewById(R.id.fragment_root));
+
+        // Scroll to the specified offset after layout
+        Bundle args = getArguments();
+        if (args != null && args.containsKey(ARG_SCROLL_Y)) {
+            final int scrollY = args.getInt(ARG_SCROLL_Y, 0);
+            ScrollUtils.addOnGlobalLayoutListener(recyclerView, new Runnable() {
+                @Override
+                public void run() {
+                    int offset = scrollY % flexibleSpaceImageHeight;
+                    RecyclerView.LayoutManager lm = recyclerView.getLayoutManager();
+                    if (lm != null && lm instanceof LinearLayoutManager) {
+                        ((LinearLayoutManager) lm).scrollToPositionWithOffset(0, -offset);
+                    }
+                }
+            });
+            updateFlexibleSpace(scrollY, view);
+        } else {
+            updateFlexibleSpace(0, view);
+        }
+
+        recyclerView.setScrollViewCallbacks(this);
+
+        return view;
+    }
+
+    @Override
+    public void setScrollY(int scrollY, int threshold) {
+        View view = getView();
+        if (view == null) {
+            return;
+        }
+        ObservableRecyclerView recyclerView = (ObservableRecyclerView) view.findViewById(R.id.scroll);
+        if (recyclerView == null) {
+            return;
+        }
+        View firstVisibleChild = recyclerView.getChildAt(0);
+        if (firstVisibleChild != null) {
+            int offset = scrollY;
+            int position = 0;
+            if (threshold < scrollY) {
+                int baseHeight = firstVisibleChild.getHeight();
+                position = scrollY / baseHeight;
+                offset = scrollY % baseHeight;
+            }
+            RecyclerView.LayoutManager lm = recyclerView.getLayoutManager();
+            if (lm != null && lm instanceof LinearLayoutManager) {
+                ((LinearLayoutManager) lm).scrollToPositionWithOffset(position, -offset);
+            }
+        }
+    }
+
+    @Override
+    protected void updateFlexibleSpace(int scrollY, View view) {
+        int flexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
+
+        View recyclerViewBackground = view.findViewById(R.id.list_background);
+
+        // Translate list background
+        ViewHelper.setTranslationY(recyclerViewBackground, Math.max(0, -scrollY + flexibleSpaceImageHeight));
+
+        // Also pass this event to parent Activity
+        FlexibleSpaceWithImageWithViewPagerTabActivity parentActivity =
+                (FlexibleSpaceWithImageWithViewPagerTabActivity) getActivity();
+        if (parentActivity != null) {
+            parentActivity.onScrollChanged(scrollY, (ObservableRecyclerView) view.findViewById(R.id.scroll));
+        }
+    }
+}
--- SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/MainActivity.java
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/MainActivity.java
@@ -1,69 +1,169 @@
 package seoulnightmarket.seoulnightmarket;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 
+import java.text.Collator;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
-    private static RecyclerView recyclerView;
-    private static Toolbar toolbar;
-    private static CollapsingToolbarLayout collapsingToolbarLayout;
-    int mutedColor = R.attr.colorPrimary;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private static final String CATEGORY_SAMPLES = MainActivity.class.getPackage().getName();
+    private static final String TAG_CLASS_NAME = "className";
+    private static final String TAG_DESCRIPTION = "description";
+    private static final String TAG_INTENT = "intent";
+
+    private static final Comparator<Map<String, Object>> DISPLAY_NAME_COMPARATOR = new Comparator<Map<String, Object>>() {
+        private final Collator collator = Collator.getInstance();
+
+        @Override
+        public int compare(Map<String, Object> lhs, Map<String, Object> rhs) {
+            return collator.compare(lhs.get("className"), rhs.get("className"));
+        }
+    };
+    private ListView listView;
+
+    // Quickly navigate through the examples.
+    enum Filter {
+        All,
+        FlexibleSpace,
+    }
+
+    Filter currentFilter = Filter.All;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
-        collapsingToolbarLayout.setTitle("Toolbar Animation Demo");//Set Title over collapsing toolbar layout
+        listView = (ListView) findViewById(android.R.id.list);
+        listView.setOnItemClickListener(this);
 
-
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
-                R.drawable.background);
-
-      // It will generate colors based on the image in an AsyncTask.
-        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-            @SuppressWarnings("ResourceType")
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_toolbar);
+        spinner.setAdapter(new FilterAdapter(this));
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onGenerated(Palette palette) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currentFilter = Filter.values()[position];
+                refreshData();
+            }
 
-                mutedColor = palette.getMutedColor(R.color.colorPrimary);
-                collapsingToolbarLayout.setContentScrimColor(mutedColor);
-                collapsingToolbarLayout.setStatusBarScrimColor(R.color.black_trans);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
             }
         });
-        setRecyclerView();
     }
 
-    //Setting recycler view
-    private void setRecyclerView() {
+    private void refreshData() {
+        listView.setAdapter(new SimpleAdapter(this, getData(),
+                R.layout.list_item_main,
+                new String[]{TAG_CLASS_NAME, TAG_DESCRIPTION,},
+                new int[]{R.id.className, R.id.description,}));
+    }
 
-        recyclerView = (RecyclerView)
-                findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView
-                .setLayoutManager(new LinearLayoutManager(MainActivity.this));//Linear Items
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> data = new ArrayList<>();
 
-        ArrayList<String> arrayList = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            arrayList.add("Card Item " + i);//Adding items to recycler view
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.setPackage(getApplicationContext().getPackageName());
+        mainIntent.addCategory(CATEGORY_SAMPLES);
+
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> list = pm.queryIntentActivities(mainIntent, 0);
+
+        if (list == null) {
+            return data;
         }
-        RecyclerView_Adapter adapter = new RecyclerView_Adapter(MainActivity.this, arrayList);
-        recyclerView.setAdapter(adapter);// set adapter on recyclerview
 
+        for (ResolveInfo info : list) {
+            CharSequence labelSeq = info.loadLabel(pm);
+            String label = labelSeq != null
+                    ? labelSeq.toString()
+                    : info.activityInfo.name;
+
+            String[] labelPath = label.split("/");
+
+            String nextLabel = labelPath[0];
+
+            if (labelPath.length == 1) {
+                String nameLabel = info.activityInfo.name.replace(info.activityInfo.packageName + "", "");
+                // Remove package and get simple class name
+                if (nameLabel.contains(".")) {
+                    nameLabel = nameLabel.replaceAll("[^.]*\\.", "");
+                }
+
+                // Filter logic.
+                if (currentFilter == Filter.All || nameLabel.contains(currentFilter.name())) {
+                    addItem(data,
+                            nameLabel,
+                            nextLabel,
+                            activityIntent(
+                                    info.activityInfo.applicationInfo.packageName,
+                                    info.activityInfo.name));
+                }
+            }
+        }
+
+        Collections.sort(data, DISPLAY_NAME_COMPARATOR);
+
+        return data;
+    }
+
+    protected Intent activityIntent(String pkg, String componentName) {
+        Intent result = new Intent();
+        result.setClassName(pkg, componentName);
+        return result;
+    }
+
+    protected void addItem(List<Map<String, Object>> data, String className, String description,
+                           Intent intent) {
+        Map<String, Object> temp = new HashMap<>();
+        temp.put(TAG_CLASS_NAME, className);
+        temp.put(TAG_DESCRIPTION, description);
+        temp.put(TAG_INTENT, intent);
+        data.add(temp);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Map<String, Object> map = (Map<String, Object>) parent.getItemAtPosition(position);
+
+        Intent intent = (Intent) map.get(TAG_INTENT);
+        startActivity(intent);
+    }
+
+    private class FilterAdapter extends ArrayAdapter<Filter> {
+        public FilterAdapter(Context context) {
+            super(context, android.R.layout.simple_spinner_item, Filter.values());
+            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        }
     }
 }
