1ff718c68c182155c5f259ccbe9be38ed6bec5f9
DESKTOP-0IINLS2\lisa9 <lisa960310@naver.com>
2017-09-15 20:16:38 +0900

    갈아업고 list뷰 예제로 action바 숨기기까지 성공

--- SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/MainActivity.java
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/MainActivity.java
@@ -1,169 +1,52 @@
 package seoulnightmarket.seoulnightmarket;
 
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.Spinner;
 
-import java.text.Collator;
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    private static final String CATEGORY_SAMPLES = MainActivity.class.getPackage().getName();
-    private static final String TAG_CLASS_NAME = "className";
-    private static final String TAG_DESCRIPTION = "description";
-    private static final String TAG_INTENT = "intent";
-
-    private static final Comparator<Map<String, Object>> DISPLAY_NAME_COMPARATOR = new Comparator<Map<String, Object>>() {
-        private final Collator collator = Collator.getInstance();
-
-        @Override
-        public int compare(Map<String, Object> lhs, Map<String, Object> rhs) {
-            return collator.compare(lhs.get("className"), rhs.get("className"));
-        }
-    };
-    private ListView listView;
-
-    // Quickly navigate through the examples.
-    enum Filter {
-        All,
-        FlexibleSpace,
-    }
-
-    Filter currentFilter = Filter.All;
-
+public class MainActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ObservableListView listView = (ObservableListView) findViewById(R.id.list);
+        listView.setScrollViewCallbacks(this);
 
-        listView = (ListView) findViewById(android.R.id.list);
-        listView.setOnItemClickListener(this);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_toolbar);
-        spinner.setAdapter(new FilterAdapter(this));
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                currentFilter = Filter.values()[position];
-                refreshData();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
+        ArrayList<String> items = new ArrayList<String>();
+        for (int i = 1; i <= 100; i++) {
+            items.add("Item " + i);
+        }
+        listView.setAdapter(new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, items));
     }
-
-    private void refreshData() {
-        listView.setAdapter(new SimpleAdapter(this, getData(),
-                R.layout.list_item_main,
-                new String[]{TAG_CLASS_NAME, TAG_DESCRIPTION,},
-                new int[]{R.id.className, R.id.description,}));
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll,
+                                boolean dragging) {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onDownMotionEvent() {
     }
 
-    private List<Map<String, Object>> getData() {
-        List<Map<String, Object>> data = new ArrayList<>();
-
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.setPackage(getApplicationContext().getPackageName());
-        mainIntent.addCategory(CATEGORY_SAMPLES);
-
-        PackageManager pm = getPackageManager();
-        List<ResolveInfo> list = pm.queryIntentActivities(mainIntent, 0);
-
-        if (list == null) {
-            return data;
-        }
-
-        for (ResolveInfo info : list) {
-            CharSequence labelSeq = info.loadLabel(pm);
-            String label = labelSeq != null
-                    ? labelSeq.toString()
-                    : info.activityInfo.name;
-
-            String[] labelPath = label.split("/");
-
-            String nextLabel = labelPath[0];
-
-            if (labelPath.length == 1) {
-                String nameLabel = info.activityInfo.name.replace(info.activityInfo.packageName + "", "");
-                // Remove package and get simple class name
-                if (nameLabel.contains(".")) {
-                    nameLabel = nameLabel.replaceAll("[^.]*\\.", "");
-                }
-
-                // Filter logic.
-                if (currentFilter == Filter.All || nameLabel.contains(currentFilter.name())) {
-                    addItem(data,
-                            nameLabel,
-                            nextLabel,
-                            activityIntent(
-                                    info.activityInfo.applicationInfo.packageName,
-                                    info.activityInfo.name));
-                }
-            }
-        }
-
-        Collections.sort(data, DISPLAY_NAME_COMPARATOR);
-
-        return data;
-    }
-
-    protected Intent activityIntent(String pkg, String componentName) {
-        Intent result = new Intent();
-        result.setClassName(pkg, componentName);
-        return result;
-    }
-
-    protected void addItem(List<Map<String, Object>> data, String className, String description,
-                           Intent intent) {
-        Map<String, Object> temp = new HashMap<>();
-        temp.put(TAG_CLASS_NAME, className);
-        temp.put(TAG_DESCRIPTION, description);
-        temp.put(TAG_INTENT, intent);
-        data.add(temp);
-    }
-
-    @SuppressWarnings("unchecked")
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Map<String, Object> map = (Map<String, Object>) parent.getItemAtPosition(position);
-
-        Intent intent = (Intent) map.get(TAG_INTENT);
-        startActivity(intent);
-    }
-
-    private class FilterAdapter extends ArrayAdapter<Filter> {
-        public FilterAdapter(Context context) {
-            super(context, android.R.layout.simple_spinner_item, Filter.values());
-            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        ActionBar ab = getSupportActionBar();
+        if (scrollState == ScrollState.UP) {
+            if (ab.isShowing()) {
+                ab.hide();
+            }
+        } else if (scrollState == ScrollState.DOWN) {
+            if (!ab.isShowing()) {
+                ab.show();
+            }
         }
     }
 }
