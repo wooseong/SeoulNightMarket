de64e964afa9ac204e59cf8be4c66f91f290afc5
DESKTOP-0IINLS2\lisa9 <lisa960310@naver.com>
2017-09-16 07:47:34 +0900

    flexibletab작업도중 에러 없는데 앱이 자꾸 꺼지는 형상 일어남 해결하기 위해 잠시 저장

--- SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/MainActivity.java
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/MainActivity.java
@@ -1,35 +1,71 @@
 package seoulnightmarket.seoulnightmarket;
 
+
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
 import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
+import com.github.ksoichiro.android.observablescrollview.Scrollable;
+import com.nineoldandroids.view.ViewHelper;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
+
+    private View mFlexibleSpaceView;
+    private View mToolbarView;
+    private TextView mTitleView;
+    private int mFlexibleSpaceHeight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_flexiblespacetoolbarscrollview);
 
-        ObservableListView listView = (ObservableListView) findViewById(R.id.list);
-        listView.setScrollViewCallbacks(this);
-
-        ArrayList<String> items = new ArrayList<String>();
-        for (int i = 1; i <= 100; i++) {
-            items.add("Item " + i);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        listView.setAdapter(new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, items));
+        mTitleView = (TextView) findViewById(R.id.title);
+        mTitleView.setText(getTitle());
+        setTitle(null);
+
+        mFlexibleSpaceView = findViewById(R.id.flexible_space);
+        mToolbarView = findViewById(R.id.toolbar);
+
+        final ObservableScrollView scrollView = (ObservableScrollView) findViewById(R.id.scroll);
+        scrollView.setScrollViewCallbacks(this);
+
+        mFlexibleSpaceHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_height);
+        int flexibleSpaceAndToolbarHeight = mFlexibleSpaceHeight + getActionBarSize();
+
+        findViewById(R.id.body).setPadding(0, flexibleSpaceAndToolbarHeight, 0, 0);
+        mFlexibleSpaceView.getLayoutParams().height = flexibleSpaceAndToolbarHeight;
+
+        ScrollUtils.addOnGlobalLayoutListener(mTitleView, new Runnable() {
+            @Override
+            public void run() {
+                updateFlexibleSpaceText(scrollView.getCurrentScrollY());
+            }
+        });
     }
+
     @Override
-    public void onScrollChanged(int scrollY, boolean firstScroll,
-                                boolean dragging) {
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        updateFlexibleSpaceText(scrollY);
     }
 
     @Override
@@ -38,15 +74,76 @@ public class MainActivity extends AppCompatActivity implements ObservableScrollV
 
     @Override
     public void onUpOrCancelMotionEvent(ScrollState scrollState) {
-        ActionBar ab = getSupportActionBar();
-        if (scrollState == ScrollState.UP) {
-            if (ab.isShowing()) {
-                ab.hide();
-            }
-        } else if (scrollState == ScrollState.DOWN) {
-            if (!ab.isShowing()) {
-                ab.show();
-            }
-        }
     }
+
+    private void updateFlexibleSpaceText(final int scrollY) {
+        ViewHelper.setTranslationY(mFlexibleSpaceView, -scrollY);
+
+        // Calculate scale
+        float maxScale = (float) (mFlexibleSpaceHeight - mToolbarView.getHeight()) / mToolbarView.getHeight();
+        int adjustedScrollY = (int) ScrollUtils.getFloat(scrollY, 0, mFlexibleSpaceHeight);
+        float scale = maxScale * ((float) mFlexibleSpaceHeight - adjustedScrollY) / mFlexibleSpaceHeight;
+
+        // Pivot the title view to (0, 0)
+        ViewHelper.setPivotX(mTitleView, 0);
+        ViewHelper.setPivotY(mTitleView, 0);
+
+        // Scale the title view
+        ViewHelper.setScaleX(mTitleView, 1 + scale);
+        ViewHelper.setScaleY(mTitleView, 1 + scale);
+
+        // Translate the title view
+        int maxTitleTranslationY = mToolbarView.getHeight() + mFlexibleSpaceHeight - (int) (mTitleView.getHeight() * (1 + scale));
+        int titleTranslationY = (int) (maxTitleTranslationY * ((float) mFlexibleSpaceHeight - adjustedScrollY) / mFlexibleSpaceHeight);
+        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+    }
+
+    protected int getActionBarSize() {
+        TypedValue typedValue = new TypedValue();
+        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
+        int indexOfAttrTextSize = 0;
+        TypedArray a = obtainStyledAttributes(typedValue.data, textSizeAttr);
+        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
+        a.recycle();
+        return actionBarSize;
+    }
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        ObservableListView listView = (ObservableListView) findViewById(R.id.scrollable);
+//        listView.setScrollViewCallbacks(this);
+//
+//        ArrayList<String> items = new ArrayList<String>();
+//        for (int i = 1; i <= 100; i++) {
+//            items.add("Item " + i);
+//        }
+//        listView.setAdapter(new ArrayAdapter<String>(
+//                this, android.R.layout.simple_list_item_1, items));
+//    }
+//    @Override
+//    //onScrollChanged (mListView . getCurrentScrollY (), false , false );
+//    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+//    }
+//
+//    @Override
+//    public void onDownMotionEvent() {
+//    }
+//
+//    @Override
+//    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+//        ActionBar ab = getSupportActionBar();
+//        if (scrollState == ScrollState.UP) {
+//            if (ab.isShowing()) {
+//                ab.hide();
+//            }
+//        } else if (scrollState == ScrollState.DOWN) {
+//            if (!ab.isShowing()) {
+//                ab.show();
+//            }
+//        }
+//    }
 }
