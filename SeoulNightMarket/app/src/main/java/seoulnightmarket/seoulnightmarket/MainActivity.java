720150e8abdde2d688ae39540b9ae204f17bb24a
DESKTOP-0IINLS2\lisa9 <lisa960310@naver.com>
2017-09-16 08:19:51 +0900

    flexiablespacewithimagegirdview 까지 성공

--- SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/MainActivity.java
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/MainActivity.java
@@ -1,17 +1,26 @@
 package seoulnightmarket.seoulnightmarket;
 
 
+import android.annotation.TargetApi;
+import android.content.res.Configuration;
 import android.content.res.TypedArray;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.ksoichiro.android.observablescrollview.ObservableGridView;
 import com.github.ksoichiro.android.observablescrollview.ObservableListView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
@@ -19,53 +28,121 @@ import com.github.ksoichiro.android.observablescrollview.ScrollState;
 import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
 import com.github.ksoichiro.android.observablescrollview.Scrollable;
 import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
+    private static final float MAX_TEXT_SCALE_DELTA = 0.3f;
+    private static final int NUM_OF_ITEMS = 100;
 
-    private View mFlexibleSpaceView;
-    private View mToolbarView;
+    private View mImageView;
+    private View mOverlayView;
+    private View mListBackgroundView;
     private TextView mTitleView;
-    private int mFlexibleSpaceHeight;
+    private View mFab;
+    private int mActionBarSize;
+    private int mFlexibleSpaceShowFabOffset;
+    private int mFlexibleSpaceImageHeight;
+    private int mFabMargin;
+    private boolean mFabIsShown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_flexiblespacetoolbarscrollview);
+        setContentView(R.layout.activity_main);
 
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        ActionBar ab = getSupportActionBar();
-        if (ab != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
         mTitleView = (TextView) findViewById(R.id.title);
         mTitleView.setText(getTitle());
         setTitle(null);
 
-        mFlexibleSpaceView = findViewById(R.id.flexible_space);
-        mToolbarView = findViewById(R.id.toolbar);
+        mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
+        mFlexibleSpaceShowFabOffset = getResources().getDimensionPixelSize(R.dimen.flexible_space_show_fab_offset);
+        mFabMargin = getResources().getDimensionPixelSize(R.dimen.margin_standard);
+        mActionBarSize = getActionBarSize();
 
-        final ObservableScrollView scrollView = (ObservableScrollView) findViewById(R.id.scroll);
-        scrollView.setScrollViewCallbacks(this);
+        mImageView = findViewById(R.id.image);
+        mOverlayView = findViewById(R.id.overlay);
+        ObservableGridView gridView = (ObservableGridView) findViewById(R.id.list);
+        gridView.setScrollViewCallbacks(this);
 
-        mFlexibleSpaceHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_height);
-        int flexibleSpaceAndToolbarHeight = mFlexibleSpaceHeight + getActionBarSize();
+        // Set padding view for ListView. This is the flexible space.
+        View paddingView = new View(this);
+        AbsListView.LayoutParams lp = new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT,
+                mFlexibleSpaceImageHeight);
+        paddingView.setLayoutParams(lp);
 
-        findViewById(R.id.body).setPadding(0, flexibleSpaceAndToolbarHeight, 0, 0);
-        mFlexibleSpaceView.getLayoutParams().height = flexibleSpaceAndToolbarHeight;
+        // This is required to disable header's list selector effect
+        paddingView.setClickable(true);
 
-        ScrollUtils.addOnGlobalLayoutListener(mTitleView, new Runnable() {
+        gridView.addHeaderView(paddingView);
+        setDummyData(gridView);
+
+        mFab = findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                updateFlexibleSpaceText(scrollView.getCurrentScrollY());
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "FAB is clicked", Toast.LENGTH_SHORT).show();
             }
         });
+
+        ViewHelper.setScaleX(mFab, 0);
+        ViewHelper.setScaleY(mFab, 0);
+
+        // mListBackgroundView makes ListView's background except header view.
+        mListBackgroundView = findViewById(R.id.list_background);
     }
 
     @Override
     public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
-        updateFlexibleSpaceText(scrollY);
+        // Translate overlay and image
+        float flexibleRange = mFlexibleSpaceImageHeight - mActionBarSize;
+        int minOverlayTransitionY = mActionBarSize - mOverlayView.getHeight();
+        ViewHelper.setTranslationY(mOverlayView, ScrollUtils.getFloat(-scrollY, minOverlayTransitionY, 0));
+        ViewHelper.setTranslationY(mImageView, ScrollUtils.getFloat(-scrollY / 2, minOverlayTransitionY, 0));
+
+        // Translate list background
+        ViewHelper.setTranslationY(mListBackgroundView, Math.max(0, -scrollY + mFlexibleSpaceImageHeight));
+
+        // Change alpha of overlay
+        ViewHelper.setAlpha(mOverlayView, ScrollUtils.getFloat((float) scrollY / flexibleRange, 0, 1));
+
+        // Scale title text
+        float scale = 1 + ScrollUtils.getFloat((flexibleRange - scrollY) / flexibleRange, 0, MAX_TEXT_SCALE_DELTA);
+        setPivotXToTitle();
+        ViewHelper.setPivotY(mTitleView, 0);
+        ViewHelper.setScaleX(mTitleView, scale);
+        ViewHelper.setScaleY(mTitleView, scale);
+
+        // Translate title text
+        int maxTitleTranslationY = (int) (mFlexibleSpaceImageHeight - mTitleView.getHeight() * scale);
+        int titleTranslationY = maxTitleTranslationY - scrollY;
+        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+
+        // Translate FAB
+        int maxFabTranslationY = mFlexibleSpaceImageHeight - mFab.getHeight() / 2;
+        float fabTranslationY = ScrollUtils.getFloat(
+                -scrollY + mFlexibleSpaceImageHeight - mFab.getHeight() / 2,
+                mActionBarSize - mFab.getHeight() / 2,
+                maxFabTranslationY);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            // On pre-honeycomb, ViewHelper.setTranslationX/Y does not set margin,
+            // which causes FAB's OnClickListener not working.
+            FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mFab.getLayoutParams();
+            lp.leftMargin = mOverlayView.getWidth() - mFabMargin - mFab.getWidth();
+            lp.topMargin = (int) fabTranslationY;
+            mFab.requestLayout();
+        } else {
+            ViewHelper.setTranslationX(mFab, mOverlayView.getWidth() - mFabMargin - mFab.getWidth());
+            ViewHelper.setTranslationY(mFab, fabTranslationY);
+        }
+
+        // Show/hide FAB
+        if (fabTranslationY < mFlexibleSpaceShowFabOffset) {
+            hideFab();
+        } else {
+            showFab();
+        }
     }
 
     @Override
@@ -76,28 +153,32 @@ public class MainActivity extends AppCompatActivity implements ObservableScrollV
     public void onUpOrCancelMotionEvent(ScrollState scrollState) {
     }
 
-    private void updateFlexibleSpaceText(final int scrollY) {
-        ViewHelper.setTranslationY(mFlexibleSpaceView, -scrollY);
-
-        // Calculate scale
-        float maxScale = (float) (mFlexibleSpaceHeight - mToolbarView.getHeight()) / mToolbarView.getHeight();
-        int adjustedScrollY = (int) ScrollUtils.getFloat(scrollY, 0, mFlexibleSpaceHeight);
-        float scale = maxScale * ((float) mFlexibleSpaceHeight - adjustedScrollY) / mFlexibleSpaceHeight;
-
-        // Pivot the title view to (0, 0)
-        ViewHelper.setPivotX(mTitleView, 0);
-        ViewHelper.setPivotY(mTitleView, 0);
-
-        // Scale the title view
-        ViewHelper.setScaleX(mTitleView, 1 + scale);
-        ViewHelper.setScaleY(mTitleView, 1 + scale);
-
-        // Translate the title view
-        int maxTitleTranslationY = mToolbarView.getHeight() + mFlexibleSpaceHeight - (int) (mTitleView.getHeight() * (1 + scale));
-        int titleTranslationY = (int) (maxTitleTranslationY * ((float) mFlexibleSpaceHeight - adjustedScrollY) / mFlexibleSpaceHeight);
-        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void setPivotXToTitle() {
+        Configuration config = getResources().getConfiguration();
+        if (Build.VERSION_CODES.JELLY_BEAN_MR1 <= Build.VERSION.SDK_INT
+                && config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+            ViewHelper.setPivotX(mTitleView, findViewById(android.R.id.content).getWidth());
+        } else {
+            ViewHelper.setPivotX(mTitleView, 0);
+        }
     }
 
+    private void showFab() {
+        if (!mFabIsShown) {
+            ViewPropertyAnimator.animate(mFab).cancel();
+            ViewPropertyAnimator.animate(mFab).scaleX(1).scaleY(1).setDuration(200).start();
+            mFabIsShown = true;
+        }
+    }
+
+    private void hideFab() {
+        if (mFabIsShown) {
+            ViewPropertyAnimator.animate(mFab).cancel();
+            ViewPropertyAnimator.animate(mFab).scaleX(0).scaleY(0).setDuration(200).start();
+            mFabIsShown = false;
+        }
+    }
     protected int getActionBarSize() {
         TypedValue typedValue = new TypedValue();
         int[] textSizeAttr = new int[]{R.attr.actionBarSize};
@@ -107,7 +188,106 @@ public class MainActivity extends AppCompatActivity implements ObservableScrollV
         a.recycle();
         return actionBarSize;
     }
+    protected void setDummyData(GridView gridView) {
+        gridView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getDummyData()));
+    }
+    public static ArrayList<String> getDummyData() {
+        return getDummyData(NUM_OF_ITEMS);
+    }
 
+    public static ArrayList<String> getDummyData(int num) {
+        ArrayList<String> items = new ArrayList<>();
+        for (int i = 1; i <= num; i++) {
+            items.add("Item " + i);
+        }
+        return items;
+    }
+}
+
+//    private View mFlexibleSpaceView;
+//    private View mToolbarView;
+//    private TextView mTitleView;
+//    private int mFlexibleSpaceHeight;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_flexiblespacetoolbarscrollview);
+//
+//        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+//        ActionBar ab = getSupportActionBar();
+//        if (ab != null) {
+//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        }
+//        mTitleView = (TextView) findViewById(R.id.title);
+//        mTitleView.setText(getTitle());
+//        setTitle(null);
+//
+//        mFlexibleSpaceView = findViewById(R.id.flexible_space);
+//        mToolbarView = findViewById(R.id.toolbar);
+//
+//        final ObservableScrollView scrollView = (ObservableScrollView) findViewById(R.id.scroll);
+//        scrollView.setScrollViewCallbacks(this);
+//
+//        mFlexibleSpaceHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_height);
+//        int flexibleSpaceAndToolbarHeight = mFlexibleSpaceHeight + getActionBarSize();
+//
+//        findViewById(R.id.body).setPadding(0, flexibleSpaceAndToolbarHeight, 0, 0);
+//        mFlexibleSpaceView.getLayoutParams().height = flexibleSpaceAndToolbarHeight;
+//
+//        ScrollUtils.addOnGlobalLayoutListener(mTitleView, new Runnable() {
+//            @Override
+//            public void run() {
+//                updateFlexibleSpaceText(scrollView.getCurrentScrollY());
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+//        updateFlexibleSpaceText(scrollY);
+//    }
+//
+//    @Override
+//    public void onDownMotionEvent() {
+//    }
+//
+//    @Override
+//    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+//    }
+//
+//    private void updateFlexibleSpaceText(final int scrollY) {
+//        ViewHelper.setTranslationY(mFlexibleSpaceView, -scrollY);
+//
+//        // Calculate scale
+//        float maxScale = (float) (mFlexibleSpaceHeight - mToolbarView.getHeight()) / mToolbarView.getHeight();
+//        int adjustedScrollY = (int) ScrollUtils.getFloat(scrollY, 0, mFlexibleSpaceHeight);
+//        float scale = maxScale * ((float) mFlexibleSpaceHeight - adjustedScrollY) / mFlexibleSpaceHeight;
+//
+//        // Pivot the title view to (0, 0)
+//        ViewHelper.setPivotX(mTitleView, 0);
+//        ViewHelper.setPivotY(mTitleView, 0);
+//
+//        // Scale the title view
+//        ViewHelper.setScaleX(mTitleView, 1 + scale);
+//        ViewHelper.setScaleY(mTitleView, 1 + scale);
+//
+//        // Translate the title view
+//        int maxTitleTranslationY = mToolbarView.getHeight() + mFlexibleSpaceHeight - (int) (mTitleView.getHeight() * (1 + scale));
+//        int titleTranslationY = (int) (maxTitleTranslationY * ((float) mFlexibleSpaceHeight - adjustedScrollY) / mFlexibleSpaceHeight);
+//        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+//    }
+//
+//    protected int getActionBarSize() {
+//        TypedValue typedValue = new TypedValue();
+//        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
+//        int indexOfAttrTextSize = 0;
+//        TypedArray a = obtainStyledAttributes(typedValue.data, textSizeAttr);
+//        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
+//        a.recycle();
+//        return actionBarSize;
+//    }
+//}
 
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
@@ -146,4 +326,4 @@ public class MainActivity extends AppCompatActivity implements ObservableScrollV
 //            }
 //        }
 //    }
-}
+//}
