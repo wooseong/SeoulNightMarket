cfbed88283376da9cc977162cb46e41e44ec9b72
Yookmoonsu <yookmoon@naver.com>
2017-09-18 14:00:14 +0900

    Review custom spinner 생성

--- SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/fragment/FragmentIntroduction.java
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/fragment/FragmentIntroduction.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import seoulnightmarket.seoulnightmarket.R;
 import seoulnightmarket.seoulnightmarket.adapter.IntroductionAdapter;
@@ -33,6 +35,13 @@ public class FragmentIntroduction extends Fragment {
         adapter.addItem("인포센터 오늘 밤의 낭만이 시작되는 곳", "종합 안내소 겸 상황실입니다. 운영본부와 의료지원 부스가 마련되어 있으니\n 궁금한 점이 있거나 비상 및 응급 상황 발생시 인포센터를 찾아주세요");
         adapter.addItem("푸드존 한강에서 즐기는 로맨틱한 저녁 식사", "한강과 가장 가까운 특별한 식당\n 전 세계 음식을 맛볼 수 있는 푸드존에서 낭만적인 저녁 식사를 즐겨보세요");
         adapter.addItem("셀러존 나만을 위한 특별한 작품", "다양한 분야의 핸드메이드 작가들이 반짝이는 아이디어와 솜씨로 여러분을 위한\n 세상에 단 하나뿐인 작품을 준비했습니다");
+
+        TextView textView0 = view.findViewById(R.id.introductionText0);
+        TextView textView1 = view.findViewById(R.id.introductionText1);
+        TextView textView2 = view.findViewById(R.id.introductionText2);
+        TextView textView3 = view.findViewById(R.id.introductionText3);
+        TextView textView4 = view.findViewById(R.id.introductionText4);
+        ImageView imageView = view.findViewById(R.id.introductionImage);
 
         return view;
     }
--- SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/fragment/FragmentReview.java
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/fragment/FragmentReview.java
@@ -7,18 +7,33 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import seoulnightmarket.seoulnightmarket.R;
 import seoulnightmarket.seoulnightmarket.adapter.ReviewAdapter;
+import seoulnightmarket.seoulnightmarket.adapter.ReviewSpinnerAdapter;
 
 public class FragmentReview extends Fragment {
+    private String[] nickName = {"내이름은 효스완스", "감태균입니다"};
+    private String[] review = {"치킨이 정말 끝내줘요", "나는 엔샵 귀염둥이"};
+    private String today;
+    private int[] flags = {R.drawable.onestar, R.drawable.twostar, R.drawable.threestar, R.drawable.fourstar, R.drawable.fivestar};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA); // 오늘 날짜
+        today = simpleDateFormat.format(new Date());
     }
 
     @Nullable
@@ -30,16 +45,26 @@ public class FragmentReview extends Fragment {
         ListView listView = view.findViewById(R.id.reviewListView);
         listView.setAdapter(adapter);
 
-        adapter.addItem(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.hyowan), "내이름은 효스완스",
-                ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.fivestar), "2017-09-16", "치킨이 정말 끝내줘요");
-        adapter.addItem(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.taegyun), "감태균씨",
-                ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.fivestar), "2017-09-17", "나는 엔샵 차기회장");
+        adapter.addItem(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.hyowan), nickName[0],
+                ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.fivestar), today, review[0]);
+        adapter.addItem(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.taegyun), nickName[1],
+                ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.fivestar), today, review[1]);
 
-        Button btnStar = view.findViewById(R.id.btn_star);
-        btnStar.setOnClickListener(new View.OnClickListener() { // 별점 주기 버튼
+        final EditText editText = view.findViewById(R.id.editText);
+
+        final Spinner spinnerStar = view.findViewById(R.id.spinnerStar);
+        ReviewSpinnerAdapter reviewSpinnerAdapter = new ReviewSpinnerAdapter(getActivity(), flags);
+        spinnerStar.setAdapter(reviewSpinnerAdapter);
+
+        spinnerStar.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "좋아요", Toast.LENGTH_SHORT).show();
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                spinnerStar.setBackground(null);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
 
@@ -47,7 +72,7 @@ public class FragmentReview extends Fragment {
         btnSubmit.setOnClickListener(new View.OnClickListener() { // 리뷰 남기기 버튼
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "리뷰 남기기", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), editText.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
