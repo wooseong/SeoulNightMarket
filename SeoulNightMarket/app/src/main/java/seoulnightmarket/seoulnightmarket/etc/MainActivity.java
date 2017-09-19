41e45b36b9ea0b4daafc8ab0e99710d048c538c9
Yookmoonsu <yookmoon@naver.com>
2017-09-19 14:34:30 +0900

    Intialscreen을 MainActivity로 이동

--- SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/etc/MainActivity.java
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/etc/MainActivity.java
@@ -1,18 +1,27 @@
 package seoulnightmarket.seoulnightmarket.etc;
 
-import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 import seoulnightmarket.seoulnightmarket.R;
+import seoulnightmarket.seoulnightmarket.adapter.MainAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    public int[] nightMarket = {R.drawable.home_banpo, R.drawable.home_cgc, R.drawable.home_cggj, R.drawable.home_ddp, R.drawable.home_yyd};
+    public GridView gridView;
+    MainAdapter mainAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
-
         super.onCreate(savedInstanceState);
 
         try {
@@ -21,9 +30,38 @@ public class MainActivity extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageViewInitial);
+        AnimationDrawable drawable = (AnimationDrawable) imageView.getBackground(); // 이미지뷰의 배경으로 애니메이션 객체 얻음
+        drawable.start(); // 애니메이션 시작
+
+        mainAdapter = new MainAdapter(getApplicationContext(), nightMarket); // 그리드뷰에 어댑터 연결
+        gridView = (GridView) findViewById(R.id.gridViewInitial);
+        gridView.setAdapter(mainAdapter);
     }
 
-    public void home(View v) {
-        startActivity(new Intent(MainActivity.this, InitialScreen.class));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { // 액션버튼 메뉴 액션바에 넣기
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.searchview_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) { // 검색어 완료시
+                searchView.clearFocus();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) { // 검색어 입력할 때
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
