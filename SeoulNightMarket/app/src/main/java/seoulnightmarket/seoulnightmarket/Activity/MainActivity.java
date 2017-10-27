dd4a9fdeab8d6bd92c5f8efb9479706619b11629
Yookmoonsu <yookmoon@naver.com>
2017-10-27 17:37:17 +0900

    네비게이션 드로어 레이아웃 수정

--- SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/Activity/MainActivity.java
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/Activity/MainActivity.java
@@ -11,7 +11,7 @@ import seoulnightmarket.seoulnightmarket.adapter.MainAdapter;
 import seoulnightmarket.seoulnightmarket.etc.TaskService;
 
 public class MainActivity extends AppCompatActivity {
-    public int[] nightMarket = {R.drawable.order, R.drawable.yyd_main, R.drawable.ddp, R.drawable.bp, R.drawable.cgc, R.drawable.cggj};
+    public int[] nightMarket = {R.drawable.login, R.drawable.yyd_white, R.drawable.ddp_white, R.drawable.bp_white, R.drawable.cgc_white, R.drawable.cggj_white};
     public GridView gridView;
 
     @Override
