2d6026f1221c144f1f6c4a4f02fb0ec7522ee3d3
JW_CHOI\cjw94 <cjw--@nate.com>
2017-10-31 04:46:56 +0900

    [ UI ] 시장 소개 부분 UI 수정

--- SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/adapter/IntroductionAdapter.java
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/adapter/IntroductionAdapter.java
@@ -1,16 +1,19 @@
 package seoulnightmarket.seoulnightmarket.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import seoulnightmarket.seoulnightmarket.R;
 import seoulnightmarket.seoulnightmarket.data.IntroductionListViewItem;
+import seoulnightmarket.seoulnightmarket.etc.Singleton;
 
 /**
  * Created by Yookmoonsu on 2017-09-17.
@@ -49,18 +52,42 @@ public class IntroductionAdapter extends BaseAdapter {
 
         TextView textView = convertView.findViewById(R.id.introductionlist0);
         TextView textView1 = convertView.findViewById(R.id.introductionlist1);
+        TextView textView2 = convertView.findViewById(R.id.introductionlist2);
+        ImageView imageView = convertView.findViewById(R.id.indexLineColor3);
 
         IntroductionListViewItem listViewItem = listViewItems.get(position);
 
-        textView.setText(listViewItem.getIntroductionTitle());
-        textView1.setText(listViewItem.getIntroductionContent());
+        textView.setText(listViewItem.getIntroductionHeader());
+        textView1.setText(listViewItem.getIntroductionTitle());
+        textView2.setText(listViewItem.getIntroductionContent());
+
+        switch(Singleton.getInstance().getRegion())
+        {
+            case "여의도":
+                imageView.setBackgroundColor(Color.parseColor("#FFA726"));
+                break;
+            case "DDP":
+                imageView.setBackgroundColor(Color.parseColor("#7E57C2"));
+                break;
+            case "반포":
+                imageView.setBackgroundColor(Color.parseColor("#FDD835"));
+                break;
+            case "청계천":
+                imageView.setBackgroundColor(Color.parseColor("#CDDC39"));
+                break;
+            case "청계광장":
+                imageView.setBackgroundColor(Color.parseColor("#00838F"));
+                break;
+        }
 
         return convertView;
     }
 
-    public void addItem(String title, String content) { // 아이템 데이터 추가를 위한 함수
+    public void addItem(String header, String title, String content)
+    { // 아이템 데이터 추가를 위한 함수
         IntroductionListViewItem item = new IntroductionListViewItem();
 
+        item.setIntroductionHeader(header);
         item.setIntroductionTitle(title);
         item.setIntroductionContent(content);
 
--- SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/fragment/FragmentIntroduction.java
+++ SeoulNightMarket/app/src/main/java/seoulnightmarket/seoulnightmarket/fragment/FragmentIntroduction.java
@@ -1,5 +1,6 @@
 package seoulnightmarket.seoulnightmarket.fragment;
 
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -19,7 +20,8 @@ import seoulnightmarket.seoulnightmarket.adapter.IntroductionAdapter;
 import seoulnightmarket.seoulnightmarket.etc.HttpTask;
 import seoulnightmarket.seoulnightmarket.etc.Singleton;
 
-public class FragmentIntroduction extends Fragment {
+public class FragmentIntroduction extends Fragment
+{
     String region;
     String uri;
     View view;
@@ -30,13 +32,18 @@ public class FragmentIntroduction extends Fragment {
     TextView textView2;
     TextView textView3;
     TextView textView4;
+    TextView textView5;
+    TextView textView6;
     ImageView imageView;
     ImageView imageView1;
+    ImageView imageColorView;
+    ImageView imageColorView1;
+    ImageView imageColorView2;
     ArrayList<String> adapterHead = new ArrayList<String>();
+    ArrayList<String> adapterTitle = new ArrayList<String>();
     ArrayList<String> adapterString = new ArrayList<String>();
 
     public FragmentIntroduction() {
-
     }
 
     @Override
@@ -46,7 +53,8 @@ public class FragmentIntroduction extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) { // onCreate 후에 화면을 구성할때 호출
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    { // onCreate 후에 화면을 구성할때 호출
         view = inflater.inflate(R.layout.activity_fragment_introduction, null);
         region = Singleton.getInstance().getRegion();
 
@@ -56,117 +64,183 @@ public class FragmentIntroduction extends Fragment {
         textView2 = view.findViewById(R.id.introductionText2);
         textView3 = view.findViewById(R.id.introductionText3);
         textView4 = view.findViewById(R.id.introductionText4);
+        textView5 = view.findViewById(R.id.introductionTimeText0);
+        textView6 = view.findViewById(R.id.introductionTimeText1);
         imageView = view.findViewById(R.id.introductionHeaderImage);
         imageView1 = view.findViewById(R.id.introductionFooterImage);
+        imageColorView = view.findViewById(R.id.indexLineColor0);
+        imageColorView1 = view.findViewById(R.id.indexLineColor1);
+        imageColorView2 = view.findViewById(R.id.indexLineColor2);
 
         adapter = new IntroductionAdapter();
         adapterHead.clear();
+        adapterTitle.clear();
         adapterString.clear();
 
-        if (region.equals("여의도")) {
+        if (region.equals("여의도"))
+        {
             textView0.setText(R.string.textView0y);
             textView1.setText(R.string.textView1y);
             textView2.setText(R.string.textView2y);
             textView3.setText(R.string.textView3y);
             textView4.setText(R.string.textView4y);
+            textView5.setText(R.string.timeView0y);
+            textView6.setText(R.string.timeView1y);
             imageView.setImageResource(R.drawable.yydintro);
             imageView1.setImageResource(R.drawable.yydintro1);
+            imageColorView.setBackgroundColor(Color.parseColor("#FFA726"));
+            imageColorView1.setBackgroundColor(Color.parseColor("#FFA726"));
+            imageColorView2.setBackgroundColor(Color.parseColor("#FFA726"));
 
             adapterHead.add(getString(R.string.listView0y));
-            adapterHead.add(getString(R.string.listView2y));
-            adapterHead.add(getString(R.string.listView4y));
+            adapterHead.add(getString(R.string.listView3y));
             adapterHead.add(getString(R.string.listView6y));
-            adapterHead.add(getString(R.string.listView8y));
+            adapterHead.add(getString(R.string.listView9y));
+            adapterHead.add(getString(R.string.listView12y));
 
-            adapterString.add(getString(R.string.listView1y));
-            adapterString.add(getString(R.string.listView3y));
+            adapterTitle.add(getString(R.string.listView1y));
+            adapterTitle.add(getString(R.string.listView4y));
+            adapterTitle.add(getString(R.string.listView7y));
+            adapterTitle.add(getString(R.string.listView10y));
+            adapterTitle.add(getString(R.string.listView13y));
+
+            adapterString.add(getString(R.string.listView2y));
             adapterString.add(getString(R.string.listView5y));
-            adapterString.add(getString(R.string.listView7y));
-            adapterString.add(getString(R.string.listView9y));
-        } else if (region.equals("DDP")) {
+            adapterString.add(getString(R.string.listView8y));
+            adapterString.add(getString(R.string.listView11y));
+            adapterString.add(getString(R.string.listView14y));
+        }
+        else if (region.equals("DDP"))
+        {
             textView0.setText(R.string.textView0d);
             textView1.setText(R.string.textView1d);
             textView2.setText(R.string.textView2d);
             textView3.setText(R.string.textView3d);
             textView4.setText(R.string.textView4d);
+            textView5.setText(R.string.timeView0d);
+            textView6.setText(R.string.timeView1d);
             imageView.setImageResource(R.drawable.ddpintro);
             imageView1.setImageResource(R.drawable.ddpintro1);
+            imageColorView.setBackgroundColor(Color.parseColor("#7E57C2"));
+            imageColorView1.setBackgroundColor(Color.parseColor("#7E57C2"));
+            imageColorView2.setBackgroundColor(Color.parseColor("#7E57C2"));
 
             adapterHead.add(getString(R.string.listView0d));
-            adapterHead.add(getString(R.string.listView2d));
-            adapterHead.add(getString(R.string.listView4d));
+            adapterHead.add(getString(R.string.listView3d));
             adapterHead.add(getString(R.string.listView6d));
-            adapterHead.add(getString(R.string.listView8d));
-            adapterHead.add(getString(R.string.listView10d));
+            adapterHead.add(getString(R.string.listView9d));
+            adapterHead.add(getString(R.string.listView12d));
+            adapterHead.add(getString(R.string.listView15d));
 
-            adapterString.add(getString(R.string.listView1d));
-            adapterString.add(getString(R.string.listView3d));
+            adapterTitle.add(getString(R.string.listView1d));
+            adapterTitle.add(getString(R.string.listView4d));
+            adapterTitle.add(getString(R.string.listView7d));
+            adapterTitle.add(getString(R.string.listView10d));
+            adapterTitle.add(getString(R.string.listView13d));
+            adapterTitle.add(getString(R.string.listView16d));
+
+            adapterString.add(getString(R.string.listView2d));
             adapterString.add(getString(R.string.listView5d));
-            adapterString.add(getString(R.string.listView7d));
-            adapterString.add(getString(R.string.listView9d));
+            adapterString.add(getString(R.string.listView8d));
             adapterString.add(getString(R.string.listView11d));
+            adapterString.add(getString(R.string.listView14d));
+            adapterString.add(getString(R.string.listView17d));
+
         } else if (region.equals("반포")) {
             textView0.setText(R.string.textView0b);
             textView1.setText(R.string.textView1b);
             textView2.setText(R.string.textView2b);
             textView3.setText(R.string.textView3b);
             textView4.setText(R.string.textView4b);
+            textView5.setText(R.string.timeView0b);
+            textView6.setText(R.string.timeView1b);
             imageView.setImageResource(R.drawable.bpintro);
             imageView1.setImageResource(R.drawable.bpintro1);
+            imageColorView.setBackgroundColor(Color.parseColor("#FDD835"));
+            imageColorView1.setBackgroundColor(Color.parseColor("#FDD835"));
+            imageColorView2.setBackgroundColor(Color.parseColor("#FDD835"));
 
             adapterHead.add(getString(R.string.listView0b));
-            adapterHead.add(getString(R.string.listView2b));
-            adapterHead.add(getString(R.string.listView4b));
+            adapterHead.add(getString(R.string.listView3b));
             adapterHead.add(getString(R.string.listView6b));
+            adapterHead.add(getString(R.string.listView9b));
 
-            adapterString.add(getString(R.string.listView1b));
-            adapterString.add(getString(R.string.listView3b));
+            adapterTitle.add(getString(R.string.listView1b));
+            adapterTitle.add(getString(R.string.listView4b));
+            adapterTitle.add(getString(R.string.listView7b));
+            adapterTitle.add(getString(R.string.listView10b));
+
+            adapterString.add(getString(R.string.listView2b));
             adapterString.add(getString(R.string.listView5b));
-            adapterString.add(getString(R.string.listView7b));
+            adapterString.add(getString(R.string.listView8b));
+            adapterString.add(getString(R.string.listView11b));
         } else if (region.equals("청계천")) {
             textView0.setText(R.string.textView0c);
             textView1.setText(R.string.textView1c);
             textView2.setText(R.string.textView2c);
             textView3.setText(R.string.textView3c);
             textView4.setText(R.string.textView4c);
+            textView5.setText(R.string.timeView0c);
+            textView6.setText(R.string.timeView1c);
             imageView.setImageResource(R.drawable.cgcintro);
             imageView1.setImageResource(R.drawable.cgcintro1);
+            imageColorView.setBackgroundColor(Color.parseColor("#CDDC39"));
+            imageColorView1.setBackgroundColor(Color.parseColor("#CDDC39"));
+            imageColorView2.setBackgroundColor(Color.parseColor("#CDDC39"));
 
             adapterHead.add(getString(R.string.listView0c));
-            adapterHead.add(getString(R.string.listView2c));
-            adapterHead.add(getString(R.string.listView4c));
+            adapterHead.add(getString(R.string.listView3c));
             adapterHead.add(getString(R.string.listView6c));
-            adapterHead.add(getString(R.string.listView8c));
+            adapterHead.add(getString(R.string.listView9c));
+            adapterHead.add(getString(R.string.listView12c));
 
-            adapterString.add(getString(R.string.listView1c));
-            adapterString.add(getString(R.string.listView3c));
+            adapterTitle.add(getString(R.string.listView1c));
+            adapterTitle.add(getString(R.string.listView4c));
+            adapterTitle.add(getString(R.string.listView7c));
+            adapterTitle.add(getString(R.string.listView10c));
+            adapterTitle.add(getString(R.string.listView13c));
+
+            adapterString.add(getString(R.string.listView2c));
             adapterString.add(getString(R.string.listView5c));
-            adapterString.add(getString(R.string.listView7c));
-            adapterString.add(getString(R.string.listView9c));
+            adapterString.add(getString(R.string.listView8c));
+            adapterString.add(getString(R.string.listView11c));
+            adapterString.add(getString(R.string.listView14c));
+
         } else if (region.equals("청계광장")) {
             textView0.setText(R.string.textView0j);
             textView1.setText(R.string.textView1j);
             textView2.setText(R.string.textView2j);
             textView3.setText(R.string.textView3j);
             textView4.setText(R.string.textView4j);
+            textView5.setText(R.string.timeView0j);
+            textView5.setText(R.string.timeView1j);
             imageView.setImageResource(R.drawable.cggjintro);
             imageView1.setImageResource(R.drawable.cggjintro1);
+            imageColorView.setBackgroundColor(Color.parseColor("#00838F"));
+            imageColorView1.setBackgroundColor(Color.parseColor("#00838F"));
+            imageColorView2.setBackgroundColor(Color.parseColor("#00838F"));
 
             adapterHead.add(getString(R.string.listView0j));
-            adapterHead.add(getString(R.string.listView2j));
-            adapterHead.add(getString(R.string.listView4j));
+            adapterHead.add(getString(R.string.listView3j));
             adapterHead.add(getString(R.string.listView6j));
-            adapterHead.add(getString(R.string.listView8j));
+            adapterHead.add(getString(R.string.listView9j));
+            adapterHead.add(getString(R.string.listView12j));
 
-            adapterString.add(getString(R.string.listView1j));
-            adapterString.add(getString(R.string.listView3j));
+            adapterTitle.add(getString(R.string.listView1j));
+            adapterTitle.add(getString(R.string.listView4j));
+            adapterTitle.add(getString(R.string.listView7j));
+            adapterTitle.add(getString(R.string.listView10j));
+            adapterTitle.add(getString(R.string.listView13j));
+
+            adapterString.add(getString(R.string.listView2j));
             adapterString.add(getString(R.string.listView5j));
-            adapterString.add(getString(R.string.listView7j));
-            adapterString.add(getString(R.string.listView9j));
+            adapterString.add(getString(R.string.listView8j));
+            adapterString.add(getString(R.string.listView11j));
+            adapterString.add(getString(R.string.listView14j));
         }
 
         for (int i = 0; i < adapterHead.size(); i++) {
-            adapter.addItem(adapterHead.get(i), adapterString.get(i));
+            adapter.addItem(adapterHead.get(i), adapterTitle.get(i), adapterString.get(i));
         }
 
         listView.setAdapter(adapter);
@@ -174,82 +248,11 @@ public class FragmentIntroduction extends Fragment {
         listView.invalidateViews();
 
         setListViewHeightBasedOnItems(listView);
-
-        // onCreate 후에 화면을 구성할때 호출
-//        uri = Uri.parse("http://ec2-13-59-247-200.us-east-2.compute.amazonaws.com:3000/intro")
-//                .buildUpon()
-//                .appendQueryParameter("place", HttpTask.getInstance().getURLEncode(region))
-//                .build().toString();
-//
-//        HttpAsyncTask httpAsyncTask = new HttpAsyncTask("소개");
-//        httpAsyncTask.execute(uri);
-
         return view;
     }
 
-    public class HttpAsyncTask extends AsyncTask<String, Void, String> {
-        int count;
-        int beforeIndex;
-        String type;
-
-        HttpAsyncTask(String type) {
-            this.type = type;
-        }
-
-        @Override
-        protected String doInBackground(String... urls) {
-            //urls[0] 은 URL 주소
-            return HttpTask.getInstance().GET(urls[0], type);
-        }
-        // onPostExecute displays the results of the AsyncTask.
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            Singleton.getInstance().getStoreImageView().setImageBitmap(HttpTask.getInstance().translateBitmap(Singleton.getInstance().getOutlineSource()));
-
-            textView0.setText(Singleton.getInstance().getOutlineTitle());
-            textView1.setText(Singleton.getInstance().getOutlineSubtitle().replace("/", "\n"));
-            textView2.setText(Singleton.getInstance().getOutlineDescribe().replace("/", "\n"));
-            textView3.setText(Singleton.getInstance().getFormTitle());
-            textView4.setText(Singleton.getInstance().getFormSubtitle().replace("/", "\n"));
-            imageView.setImageBitmap(HttpTask.getInstance().translateBitmap(Singleton.getInstance().getOutlineSource()));
-            imageView1.setImageBitmap(HttpTask.getInstance().translateBitmap(Singleton.getInstance().getFormSource()));
-
-            adapter = new IntroductionAdapter();
-
-            adapterHead.clear();
-            adapterString.clear();
-            count = 1;
-            beforeIndex = 0;
-
-            for (int i = 0; i < Singleton.getInstance().getFormDescribe().length(); i++) {
-                if (Singleton.getInstance().getFormDescribe().substring(i, i + 1).equals("/")) {
-                    if (count % 2 == 1) {
-                        adapterHead.add(Singleton.getInstance().getFormDescribe().substring(beforeIndex, i));
-                    } else {
-                        adapterString.add(Singleton.getInstance().getFormDescribe().substring(beforeIndex, i));
-                    }
-
-                    beforeIndex = i + 1;
-                    count++;
-                }
-            }
-
-            for (int i = 0; i < adapterHead.size(); i++) {
-                adapter.addItem(adapterHead.get(i), adapterString.get(i).replace("=", "\n"));
-            }
-
-            listView.setAdapter(adapter);
-            adapter.notifyDataSetChanged(); // 데이터 변경시 리스트뷰 갱신
-            listView.invalidateViews();
-
-            setListViewHeightBasedOnItems(listView);
-        }
-    }
-
-    public void setListViewHeightBasedOnItems(ListView listView) { // 리스트뷰 높이 계산
+    public void setListViewHeightBasedOnItems(ListView listView)
+    { // 리스트뷰 높이 계산
         ListAdapter listAdapter = listView.getAdapter();
 
         if (listAdapter == null) return;
